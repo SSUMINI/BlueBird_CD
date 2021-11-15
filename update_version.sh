c4a347526ef68e0e6c82fae1259afec6fbb9e354
minseok <alstjrsung@gmail.com>
2021-11-15 15:47:14 +0900

    init commit

---
+++ update_version.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+TAG_NAME=$(wget -q https://registry.hub.docker.com/v1/repositories/sms0430/bluebird/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | tail -1)
+
+echo $TAG_NAME
+
+#sed -i "22s/mywebapp.*$/mywebapp:$TAG_NAME/g" deploy.yaml
+
+exit 0

334053be9cf75e6405aa867de9d93ecee58dad6d
minseok <alstjrsung@gmail.com>
2021-11-22 17:07:26 +0900

    a

--- update_version.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-TAG_NAME=$(wget -q https://registry.hub.docker.com/v1/repositories/sms0430/bluebird/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | tail -1)
-
-echo $TAG_NAME
-
-sed -i "22s/bluebird.*$/bluebird:$TAG_NAME/g" ./bluebird/deploy.yaml
-
-exit 0
